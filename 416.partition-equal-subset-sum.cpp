afb30c29ccd943b14ba36a483252c585f86b66df
Suraj Meena <surajmeena505@gmail.com>
2022-08-20 14:50:39 +0530

    416 - Recursive approach done

--- 416.partition-equal-subset-sum.cpp
+++ 416.partition-equal-subset-sum.cpp
@@ -12,18 +12,18 @@ public:
     {
         if (totalSum - currSum == currSum)
         {
-            cout << currSum << " Here it got returned true" << endl;
+            // cout << currSum << " Here it got returned true" << endl;
             return true;
         }
         else if (currInd >= nums.size())
         {
-            cout << currInd << " " << nums.size() << endl;
-            cout << currSum << " Here it exceeded arr length" << endl;
+            // cout << currInd << " " << nums.size() << endl;
+            // cout << currSum << " Here it exceeded arr length" << endl;
             return false;
         }
         // else
-        cout << currSum << endl;
-        for (int i = currInd + 1; i < nums.size(); i++)
+        // cout << currSum << endl;
+        for (int i = currInd; i < nums.size(); i++)
         {
             if (recurse(currSum + nums[i], i + 1, nums, totalSum) or recurse(currSum, i + 1, nums, totalSum))
                 return true;
@@ -35,7 +35,7 @@ public:
         // try to form all possible combinations through recursion
         // and keep track of their sum rather than the combination itself
         int totalSum = accumulate(nums.begin(), nums.end(), 0);
-        return recurse(0, -1, nums, totalSum);
+        return recurse(0, 0, nums, totalSum);
     }
 };
 // @lc code=end
