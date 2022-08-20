d32dfba6dfda2cf4644a6058078b5139c6bce813
Suraj Meena <surajmeena505@gmail.com>
2022-08-20 13:24:05 +0530

    416 - partition equal subset sum
    - having some issue with my first approach, debug it with help of @neha
    - when `currInd=-1` then `currInd>=nums.size()` is getting satisfied which shouldn't be possible

---
+++ 416.partition-equal-subset-sum.cpp
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode id=416 lang=cpp
+ *
+ * [416] Partition Equal Subset Sum
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    bool recurse(int currSum, int currInd, vector<int> &nums, int totalSum)
+    {
+        if (totalSum - currSum == currSum)
+        {
+            cout << currSum << " Here it got returned true" << endl;
+            return true;
+        }
+        else if (currInd >= nums.size())
+        {
+            cout << currInd << " " << nums.size() << endl;
+            cout << currSum << " Here it exceeded arr length" << endl;
+            return false;
+        }
+        // else
+        cout << currSum << endl;
+        for (int i = currInd + 1; i < nums.size(); i++)
+        {
+            if (recurse(currSum + nums[i], i + 1, nums, totalSum) or recurse(currSum, i + 1, nums, totalSum))
+                return true;
+        }
+        return false;
+    }
+    bool canPartition(vector<int> &nums)
+    {
+        // try to form all possible combinations through recursion
+        // and keep track of their sum rather than the combination itself
+        int totalSum = accumulate(nums.begin(), nums.end(), 0);
+        return recurse(0, -1, nums, totalSum);
+    }
+};
+// @lc code=end
